a1bf4fd1d49db51d4cfbc78abc40afb52b410172
johnpezley <jpez.cyfan@gmail.com>
2026-09-15 20:14:27 -0500

    Remove left sidebar navigation rendering
    Removed the rendering of the left sidebar navigation.

--- nav.js
+++ nav.js
@@ -19,17 +19,4 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
         `;
     }
-
-    // 2. Render Left Sidebar Navigation
-    const sidebarNavElement = document.getElementById("sidebar-nav-component");
-    if (sidebarNavElement) {
-        sidebarNavElement.innerHTML = `
-            <h3>Primary Navigation</h3>
-            <ul>
-                <li><a href="index.html">Dashboard</a></li>
-                <li><a href="#">Repositories</a></li>
-                <li><a href="#">Settings</a></li>
-            </ul>
-        `;
-    }
 });
