2c33f5b1b6d0e8b9bc5ae69bf23b344f0df6fd44
johnpezley <jpez.cyfan@gmail.com>
2026-09-19 20:30:29 -0500

    Fix typo in navigation link text

--- nav.js
+++ nav.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <ul>
                 <li><a href="index.html">Dashboard</a></li>
                 <li><a href="repositories.html">Repositories</a></li>
-                <li><a href="kermit.html">Picure of Kermit</a></li>
+                <li><a href="kermit.html">Picture of Kermit</a></li>
             </ul>
         `;
     }
